5f242bba65974a0ec4f26ef70e5516ab10296db9
Jon Hecht <jhecht@smartassdesign.com>
2016-05-10 12:05:37 -0400

    initial nav options

--- library/js/app.js
+++ library/js/app.js
@@ -15,3 +15,40 @@ $('.db-slider').each(function() {
   });
 });
 });
+
+
+// AFFIX NAV JS
+
+
+
+
+// BURGER JS
+
+
+var clickDelay      = 500,
+    clickDelayTimer = null;
+
+$('.burger-click-region').on('click', function () {
+  
+  if(clickDelayTimer === null) {
+  
+    var $burger = $(this);
+    $burger.toggleClass('active');
+    $burger.parent().toggleClass('is-open');
+
+    if(!$burger.hasClass('active')) {
+      $burger.addClass('closing');
+    }
+
+    clickDelayTimer = setTimeout(function () {
+      $burger.removeClass('closing');
+      clearTimeout(clickDelayTimer);
+      clickDelayTimer = null;
+    }, clickDelay);
+  }
+});
+
+
+
+
+
