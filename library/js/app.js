c03a3338f1fc67360e3a522570133f68bf626eff
Jon Hecht <jhecht@smartassdesign.com>
2016-05-10 13:54:44 -0400

    new navs

--- library/js/app.js
+++ library/js/app.js
@@ -25,28 +25,30 @@ $('.db-slider').each(function() {
 // BURGER JS
 
 
-var clickDelay      = 500,
-    clickDelayTimer = null;
 
-$('.burger-click-region').on('click', function () {
+
+// var clickDelay      = 500,
+//     clickDelayTimer = null;
+
+// $('.burger-click-region').on('click', function () {
   
-  if(clickDelayTimer === null) {
+//   if(clickDelayTimer === null) {
   
-    var $burger = $(this);
-    $burger.toggleClass('active');
-    $burger.parent().toggleClass('is-open');
+//     var $burger = $(this);
+//     $burger.toggleClass('active');
+//     $burger.parent().toggleClass('is-open');
 
-    if(!$burger.hasClass('active')) {
-      $burger.addClass('closing');
-    }
+//     if(!$burger.hasClass('active')) {
+//       $burger.addClass('closing');
+//     }
 
-    clickDelayTimer = setTimeout(function () {
-      $burger.removeClass('closing');
-      clearTimeout(clickDelayTimer);
-      clickDelayTimer = null;
-    }, clickDelay);
-  }
-});
+//     clickDelayTimer = setTimeout(function () {
+//       $burger.removeClass('closing');
+//       clearTimeout(clickDelayTimer);
+//       clickDelayTimer = null;
+//     }, clickDelay);
+//   }
+// });
 
 
 
